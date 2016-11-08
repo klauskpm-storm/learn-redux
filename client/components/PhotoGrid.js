5cf3d218cc92a73e93d47e7b793bd732213ae14b
Klaus Kazlauskas <klaus.machado@stormsec.com.br>
2016-11-08 14:41:01 -0200

    Handling the posts response
     - Passing it through PhotoGrid to Photo;
     - Creating a relevant UI for photo;

--- client/components/PhotoGrid.js
+++ client/components/PhotoGrid.js
@@ -2,12 +2,23 @@
  * Created by klaus on 04/11/16.
  */
 import React, { Component } from 'react';
+import Photo from './Photo';
 
 export default class PhotoGrid extends Component {
+    renderPhoto (post, i) {
+        return (
+            <Photo
+                {...this.props}
+                key={i}
+                post={post}
+                i={i} />
+        );
+    }
+
     render () {
         return (
             <div className="photo-grid">
-                I'm the photo grid
+                {this.props.posts.map(this.renderPhoto.bind(this))}
             </div>
         )
     }
