e1b58751ec743a801058946b569ef8650f0c4c8d
Klaus Kazlauskas <klaus.machado@stormsec.com.br>
2016-11-08 15:58:28 -0200

    Creating and including the comment react component

---
+++ client/components/Comments.js
@@ -0,0 +1,16 @@
+/**
+ * Created by klaus on 08/11/16.
+ */
+import React, { Component } from 'react';
+
+class Comments extends Component {
+    render () {
+        return (
+            <div className="comment">
+                I'm the comments!
+            </div>
+        )
+    }
+}
+
+export default Comments;
--- client/components/Single.js
+++ client/components/Single.js
@@ -2,12 +2,25 @@
  * Created by klaus on 04/11/16.
  */
 import React, { Component } from 'react';
+import Photo from './Photo';
+import Comments from './Comments';
 
 export default class Single extends Component {
+    isCurrentPost (post) {
+        return post.code === this.props.params.postId;
+    }
+
     render () {
+        let i = this.props.posts.findIndex(this.isCurrentPost.bind(this));
+        let post = this.props.posts[i];
+
         return (
             <div className="single-photo">
-                I'm the single
+                <Photo
+                    post={post}
+                    i={i}
+                    {...this.props} />
+                <Comments/>
             </div>
         )
     }
